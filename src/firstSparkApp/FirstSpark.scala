0652945a4fe5b3b99f13e4dd0b11b3a1fd502e2b
canran gou <gycrazydog@gmail.com>
2015-04-28 21:31:22 +0200

    first commit

---
+++ src/firstSparkApp/FirstSpark.scala
@@ -0,0 +1,27 @@
+package firstSparkApp
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.SparkConf
+import org.apache.spark._
+import org.apache.spark.graphx._
+// To make some of the examples work we will also need RDD
+import org.apache.spark.rdd.RDD
+import org.apache.spark.graphx.PartitionStrategy._
+object FirstSpark {
+  def main(args:Array[String]){
+    val conf = new SparkConf().setAppName("first spark app!").setMaster("local[4]")
+    val sc = new SparkContext(conf)
+    val users: RDD[(VertexId, (String, String))] =
+    sc.parallelize(Array((3L, ("rxin", "student")), (7L, ("jgonzal", "postdoc")),
+                       (5L, ("franklin", "prof")), (2L, ("istoica", "prof"))))
+    // Create an RDD for edges
+    val relationships: RDD[Edge[String]] =
+      sc.parallelize(Array(Edge(3L, 7L, "collab"),    Edge(5L, 3L, "advisor"),
+                           Edge(2L, 5L, "colleague"), Edge(5L, 7L, "pi")))
+    // Define a default user in case there are relationship with missing user
+    val defaultUser = ("John Doe", "Missing")
+    // Build the initial Graph
+    val graph = Graph(users, relationships, defaultUser)
+    graph.partitionBy(EdgePartition1D, 3)
+  }
+}
