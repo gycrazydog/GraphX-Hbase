5b82199ad386598f18f6508be65071622d698114
canran gou <gycrazydog@gmail.com>
2015-04-30 20:10:58 +0200

    successfully connected hbase

--- src/firstSparkApp/SparkOnHbaseTest.scala
+++ src/firstSparkApp/SparkOnHbaseTest.scala
@@ -12,11 +12,13 @@ import org.apache.hadoop.hbase.util.Bytes
 import org.apache.hadoop.hbase.HBaseConfiguration
 import org.apache.hadoop.hbase.client.Put
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.hbase.client.Scan
 object SparkOnHbaseTest {
   def main(args : Array[String]){
     // Nothing to see here just creating a SparkContext like you normally would
-  val sparkConf = new SparkConf().setAppName("first spark app!").setMaster("local[3]")
+  val sparkConf = new SparkConf().setAppName("first spark app!").setMaster("local[2]")
   val sc = new SparkContext(sparkConf)
+  println("after=enter!!!!")
   val columnFamily = "testColumn"
   //This is making a RDD of
   //(RowKey, columnFamily, columnQualifier, value)
@@ -31,22 +33,32 @@ object SparkOnHbaseTest {
    
   //Create the HBase config like you normally would  then
   //Pass the HBase configs and SparkContext to the HBaseContext
-  val conf = HBaseConfiguration.create();
-      conf.addResource(new Path("/usr/lib/hbase/hbase-0.94.27/conf/core-site.xml"));
-      conf.addResource(new Path("/usr/lib/hbase/hbase-0.94.27/conf/hbase-site.xml"));
-  val hbaseContext = new HBaseContext(sc, conf);
-   
+  val conf = HBaseConfiguration.create()
+//      conf.addResource(new Path("/usr/lib/hbase/hbase-0.94.27/conf/core-site.xml"))
+      conf.addResource(new Path("/home/crazydog/hbase-0.98.12-hadoop2/conf/hbase-site.xml"))
+  val hbaseContext = new HBaseContext(sc, conf)
   //Now give the rdd, table name, and a function that will convert a RDD record to a put, and finally
   // A flag if you want the puts to be batched
-  hbaseContext.bulkPut[(Array[Byte], Array[(Array[Byte], Array[Byte], Array[Byte])])](rdd,
+//  var scan = new Scan()
+//    scan.setCaching(100)
+//
+//    var getRdd = hbaseContext.hbaseRDD( "SparkTest", scan)
+//    println(" --- abc")
+//    getRdd.foreach(v => println(Bytes.toString(v._1)))
+//    println(" --- def")
+//    getRdd.collect.foreach(v => println(Bytes.toString(v._1)))
+//    println(" --- qwe")
+    println("before put")
+    hbaseContext.bulkPut[(Array[Byte], Array[(Array[Byte], Array[Byte], Array[Byte])])](rdd,
       "SparkTest",
       //This function is really important because it allows our source RDD to have data of any type
       // Also because puts are not serializable
       (putRecord) => {
         val put = new Put(putRecord._1)
         putRecord._2.foreach((putValue) => put.add(putValue._1, putValue._2, putValue._3))
-         put
+        put
       },
       true);
+      println("afterput")
     }
   }
