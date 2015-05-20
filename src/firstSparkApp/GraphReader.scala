7adc5e53ab66eb03782933a10c447f5a0e094d52
canran gou <gycrazydog@gmail.com>
2015-05-20 11:22:29 +0200

    tested for parallel algorithm

--- src/firstSparkApp/GraphReader.scala
+++ src/firstSparkApp/GraphReader.scala
@@ -4,18 +4,133 @@ import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
 import org.apache.spark._
 import org.apache.spark.graphx._
+// To make some of the examples work we will also need RDD
 import org.apache.spark.rdd.RDD
 import org.apache.spark.graphx.PartitionStrategy._
+import com.cloudera.spark.hbase.HBaseContext
+import org.apache.hadoop.hbase.util.Bytes
+import org.apache.hadoop.hbase.HBaseConfiguration
+import org.apache.hadoop.hbase.Cell
+import org.apache.hadoop.hbase.client.Put
+import org.apache.hadoop.hbase.client.Get
+import org.apache.hadoop.hbase.client.Result
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.hbase.util.Base64
+import org.apache.hadoop.hbase.client.Scan
+import scala.collection.mutable.Buffer
+import org.apache.hadoop.hbase.mapreduce.TableInputFormat
+import scala.collection.JavaConverters._
 object GraphReader {
-  def readVertices(sc : SparkContext):RDD[(VertexId, (String, String))] = sc.parallelize(Array(
-                      (3L, ("rxin", "student")), 
-                      (7L, ("jgonzal", "postdoc")),
-                       (5L, ("franklin", "prof")), 
-                       (2L, ("istoica", "prof")),
-                       (4L, ("istoica", "prof"))
+  val tableName = "TestGraph";
+  val conf = HBaseConfiguration.create()
+  conf.set(TableInputFormat.INPUT_TABLE,tableName)
+  def read(sc : SparkContext):Graph[Any,String] = {
+  
+      val hBaseRDD = sc.newAPIHadoopRDD(conf, classOf[TableInputFormat], 
+        classOf[org.apache.hadoop.hbase.io.ImmutableBytesWritable],
+        classOf[org.apache.hadoop.hbase.client.Result])
+      hBaseRDD.foreach(v => {
+        print(Bytes.toString(v._1.get()))
+        val result = v._2
+        val it = result.listCells().iterator()
+        while(it.hasNext()){
+          val kv = it.next
+          print(" "+Bytes.toString(kv.getQualifier)+" "+Bytes.toString(kv.getValue))
+        }
+        println()
+      })
+      var users : RDD[(VertexId,(Any))]= hBaseRDD.map( v=> (Bytes.toString(v._1.get()).toLong,())) 
+      
+      println("before flatmap!")
+      var relationships = hBaseRDD.flatMap(v=> {
+        val result : Result = v._2
+        val pp  = result.listCells.asScala.map ( cell => 
+          if(Bytes.toString(cell.getQualifier)!="")
+            Edge(Bytes.toString(v._1.get()).toLong, Bytes.toString(cell.getValue).toLong,Bytes.toString(cell.getQualifier)) 
+          else
+            Edge(Bytes.toString(v._1.get()).toLong, Bytes.toString(v._1.get()).toLong,Bytes.toString(cell.getQualifier)) 
+        )
+        pp  
+      })
+      val edges = relationships.filter(edge => edge.attr!="")
+      val defaultUser = ("John Doe", "Missing")
+      // Build the initial Graph
+      val graph = Graph(users, edges, defaultUser)
+      graph
+  }
+  def automata(sc : SparkContext) : Graph[Any,String] = {
+     val states : RDD[(VertexId,(Any))]= sc.parallelize(Array(
+                      (1L, ()), 
+                      (2L, ()),
+                       (3L, ())
                        ))
-  def readEdges(sc : SparkContext): RDD[Edge[String]] = sc.parallelize(Array(
-                    Edge(3L, 5L, "collab"),    
-                    Edge(3L, 2L, "advisor"),
-                    Edge(3L, 7L, "colleague")))
+      val trans = sc.parallelize(Array(
+                    Edge(1L, 2L, "category"),    
+                    Edge(2L, 3L, "Entertainment"),
+                    Edge(1L,3L,"324")
+                  ))
+      val defaultUser = ("John Doe", "Missing")
+      val graph = Graph(states, trans,defaultUser)
+      graph
+  }
+  def getEdgesByIds(sc: SparkContext,ids : RDD[VertexId]): RDD[Edge[String]] = {
+      val hbaseContext = new HBaseContext(sc, conf)
+      val inputRdd = ids.map(v=>Bytes.toBytes(v.toString()))
+      val getRdd = hbaseContext.bulkGet[Array[Byte], Result](
+          "TestGraph",  //The table we want to get from
+          2,  //Get list batch size.  Set this somewhere under 1000
+          inputRdd,  //RDD that hold records that will turn into Gets
+          record => {    //Function that will take a given record to a Get
+            new Get(record)
+          },
+          (result: Result) => {result}
+      )
+      val resRdd = getRdd.flatMap(v=> {
+        val result : Result = v
+        val pp  = result.listCells.asScala.map ( cell => 
+          if(Bytes.toString(cell.getQualifier)!="")
+            Edge(Bytes.toString(result.getRow).toLong, Bytes.toString(cell.getValue).toLong,Bytes.toString(cell.getQualifier)) 
+          else
+            Edge(Bytes.toString(result.getRow).toLong, Bytes.toString(result.getRow).toLong,Bytes.toString(cell.getQualifier)) 
+        )
+        pp  
+      })
+//      val edges = resRdd.filter(edge => edge.attr!="")
+//      println("after filter!"+edges.count())
+      resRdd
+  }
+  def getAllVerticesId(sc:SparkContext): RDD[VertexId] = {
+    val hBaseRDD = sc.newAPIHadoopRDD(conf, classOf[TableInputFormat], 
+        classOf[org.apache.hadoop.hbase.io.ImmutableBytesWritable],
+        classOf[org.apache.hadoop.hbase.client.Result])
+    hBaseRDD.map( v=> Bytes.toString(v._1.get()).toLong)
+  }
+  def getEdgesByLabels(sc:SparkContext,ColumnFamily:String,Columns : Array[String]) : RDD[Edge[String]] = {
+      println("enter getEdges!!")
+      val hbaseContext = new HBaseContext(sc, conf)
+      val arr = sc.parallelize(Array.range(1,27).map(i=>Bytes.toBytes(i.toString())))
+      val getRdd = hbaseContext.bulkGet[Array[Byte], Result](
+      "TestGraph",  //The table we want to get from
+      100,  //Get list batch size.  Set this somewhere under 1000
+      arr,  //RDD that hold records that will turn into Gets
+      record => {    //Function that will take a given record to a Get
+        var rec = new Get(record)
+        Columns.map(v=>rec.addColumn(Bytes.toBytes(ColumnFamily), Bytes.toBytes(v)))
+        rec
+      },
+      (result: Result) => {  //Function that will take a given result and return a serializable object
+        result
+      }).filter(v=>(!v.isEmpty())).flatMap(v=> {
+        val result : Result = v
+        val pp  = result.listCells.asScala.map ( cell => 
+          if(Bytes.toString(cell.getQualifier)!="")
+            Edge(Bytes.toString(result.getRow).toLong, Bytes.toString(cell.getValue).toLong,Bytes.toString(cell.getQualifier)) 
+          else
+            Edge(Bytes.toString(result.getRow).toLong, Bytes.toString(result.getRow).toLong,Bytes.toString(cell.getQualifier)) 
+        )
+        pp  
+      })
+      getRdd.cache()
+      getRdd
+  }
 }
--- src/firstSparkApp/SparkOnHbaseTest.scala
+++ src/firstSparkApp/SparkOnHbaseTest.scala
@@ -11,8 +11,12 @@ import com.cloudera.spark.hbase.HBaseContext
 import org.apache.hadoop.hbase.util.Bytes
 import org.apache.hadoop.hbase.HBaseConfiguration
 import org.apache.hadoop.hbase.client.Put
+import org.apache.hadoop.hbase.client.HTable;  
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.hbase.client.Get
+import org.apache.hadoop.hbase.client.Result
 import org.apache.hadoop.hbase.client.Scan
+import scala.collection.JavaConverters._
 object SparkOnHbaseTest {
   def main(args : Array[String]){
     // Nothing to see here just creating a SparkContext like you normally would
@@ -26,10 +30,29 @@ object SparkOnHbaseTest {
         (Bytes.toBytes("1"), Array((Bytes.toBytes(columnFamily), Bytes.toBytes("id"), Bytes.toBytes("2") ) ) ),
         (Bytes.toBytes("2"), Array((Bytes.toBytes(columnFamily), Bytes.toBytes("324"), Bytes.toBytes("3")))),
         (Bytes.toBytes("3"), Array((Bytes.toBytes(columnFamily), Bytes.toBytes("category"), Bytes.toBytes("4") ),(Bytes.toBytes(columnFamily), Bytes.toBytes("relate"), Bytes.toBytes("6") ) ) ),
-        (Bytes.toBytes("4"), Array((Bytes.toBytes(columnFamily), Bytes.toBytes("music"), Bytes.toBytes("5")))),
+        (Bytes.toBytes("4"), Array((Bytes.toBytes(columnFamily), Bytes.toBytes("Music"), Bytes.toBytes("5")))),
         (Bytes.toBytes("5"), Array((Bytes.toBytes(columnFamily), Bytes.toBytes(""), Bytes.toBytes("")))),
         (Bytes.toBytes("6"), Array((Bytes.toBytes(columnFamily), Bytes.toBytes("video"), Bytes.toBytes("7") ) ) ),
-        (Bytes.toBytes("7"), Array((Bytes.toBytes(columnFamily), Bytes.toBytes(""), Bytes.toBytes("")) ) )
+        (Bytes.toBytes("7"), Array((Bytes.toBytes(columnFamily), Bytes.toBytes("id"), Bytes.toBytes("8")) ) ),
+        (Bytes.toBytes("8"), Array((Bytes.toBytes(columnFamily), Bytes.toBytes("428"), Bytes.toBytes("9")) ) ),
+        (Bytes.toBytes("9"), Array((Bytes.toBytes(columnFamily), Bytes.toBytes("category"), Bytes.toBytes("10") ),(Bytes.toBytes(columnFamily), Bytes.toBytes("comments"), Bytes.toBytes("12") ),(Bytes.toBytes(columnFamily), Bytes.toBytes("relate"), Bytes.toBytes("14") ) ) ),
+        (Bytes.toBytes("10"), Array((Bytes.toBytes(columnFamily), Bytes.toBytes("Entertainment"), Bytes.toBytes("11")) ) ),
+        (Bytes.toBytes("11"), Array((Bytes.toBytes(columnFamily), Bytes.toBytes(""), Bytes.toBytes("")) ) ),
+        (Bytes.toBytes("12"), Array((Bytes.toBytes(columnFamily), Bytes.toBytes("39"), Bytes.toBytes("13")) ) ),
+        (Bytes.toBytes("13"), Array((Bytes.toBytes(columnFamily), Bytes.toBytes(""), Bytes.toBytes("")) ) ),
+        (Bytes.toBytes("14"), Array((Bytes.toBytes(columnFamily), Bytes.toBytes("video"), Bytes.toBytes("15")) ) ),
+        (Bytes.toBytes("15"), Array((Bytes.toBytes(columnFamily), Bytes.toBytes("id"), Bytes.toBytes("16")) ) ),
+        (Bytes.toBytes("16"), Array((Bytes.toBytes(columnFamily), Bytes.toBytes("278"), Bytes.toBytes("17")) ) ),
+        (Bytes.toBytes("17"), Array((Bytes.toBytes(columnFamily), Bytes.toBytes("relate"), Bytes.toBytes("20")),(Bytes.toBytes(columnFamily), Bytes.toBytes("relate"), Bytes.toBytes("21")),(Bytes.toBytes(columnFamily), Bytes.toBytes("category"), Bytes.toBytes("18")) ) ),
+        (Bytes.toBytes("18"), Array((Bytes.toBytes(columnFamily), Bytes.toBytes("Entertainment"), Bytes.toBytes("19")) ) ),
+        (Bytes.toBytes("19"), Array((Bytes.toBytes(columnFamily), Bytes.toBytes(""), Bytes.toBytes("")) ) ),
+        (Bytes.toBytes("20"), Array((Bytes.toBytes(columnFamily), Bytes.toBytes("video"), Bytes.toBytes("7")) ) ),
+        (Bytes.toBytes("21"), Array((Bytes.toBytes(columnFamily), Bytes.toBytes("video"), Bytes.toBytes("22")) ) ),
+        (Bytes.toBytes("22"), Array((Bytes.toBytes(columnFamily), Bytes.toBytes("id"), Bytes.toBytes("23")) ) ),
+        (Bytes.toBytes("23"), Array((Bytes.toBytes(columnFamily), Bytes.toBytes("573"), Bytes.toBytes("24")) ) ),
+        (Bytes.toBytes("24"), Array((Bytes.toBytes(columnFamily), Bytes.toBytes("category"), Bytes.toBytes("25")) ) ),
+        (Bytes.toBytes("25"), Array((Bytes.toBytes(columnFamily), Bytes.toBytes("Entertainment"), Bytes.toBytes("26")) ) ),
+        (Bytes.toBytes("26"), Array((Bytes.toBytes(columnFamily), Bytes.toBytes(""), Bytes.toBytes("")) ) )
        )
       )
    
@@ -39,17 +62,35 @@ object SparkOnHbaseTest {
 //      conf.addResource(new Path("/usr/lib/hbase/hbase-0.94.27/conf/core-site.xml"))
       conf.addResource(new Path("/usr/lib/hbase/conf/hbase-site.xml"))
   val hbaseContext = new HBaseContext(sc, conf)
-  //Now give the rdd, table name, and a function that will convert a RDD record to a put, and finally
-  // A flag if you want the puts to be batched
-//  var scan = new Scan()
-//    scan.setCaching(100)
-//
-//    var getRdd = hbaseContext.hbaseRDD( "SparkTest", scan)
+  val arr = sc.parallelize(Array.range(1,3).map(i=>Bytes.toBytes(i.toString())))
+  arr.collect().map(i=>i.map(println))
 //    println(" --- abc")
 //    getRdd.foreach(v => println(Bytes.toString(v._1)))
-//    println(" --- def")
-//    getRdd.collect.foreach(v => println(Bytes.toString(v._1)))
-//    println(" --- qwe")
+//This is the method we are going to focus on
+//    val getRdd = hbaseContext.bulkGet[Array[Byte], Result](
+//      "TestGraph",  //The table we want to get from
+//      100,  //Get list batch size.  Set this somewhere under 1000
+//      arr,  //RDD that hold records that will turn into Gets
+//      record => {    //Function that will take a given record to a Get
+//        var rec = new Get(record)
+//        rec.addColumn(Bytes.toBytes("Cells"), Bytes.toBytes("id"))
+//        rec
+//      },
+//      (result: Result) => {  //Function that will take a given result and return a serializable object
+//        println("what  "+result)
+//        result
+//      })
+//      val resRdd = getRdd.filter(v=>(!v.isEmpty())).flatMap(v=> {
+//        val result : Result = v
+//        val pp  = result.listCells.asScala.map ( cell => 
+//          if(Bytes.toString(cell.getQualifier)!="")
+//            Edge(Bytes.toString(result.getRow).toLong, Bytes.toString(cell.getValue).toLong,Bytes.toString(cell.getQualifier)) 
+//          else
+//            Edge(Bytes.toString(result.getRow).toLong, Bytes.toString(result.getRow).toLong,Bytes.toString(cell.getQualifier)) 
+//        )
+//        pp  
+//      })
+//      resRdd.collect().map(v=>println(v.toString()))
     println("before put")
     hbaseContext.bulkPut[(Array[Byte], Array[(Array[Byte], Array[Byte], Array[Byte])])](rdd,
       "TestGraph",
